e828abaefbb8b319fa4713db06655af29f8fca21
Linlin <linlin@d-i89-172-234.student.eduroam.uq.edu.au>
2020-10-12 19:26:44 +1000

    react modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,118 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+        <header className="header">
+            <div className="brand">
+                <button onclick="openMenu()">
+                    &#9776;
+                </button>
+                <a href="index.html">LOCAL TRADING</a>
+            </div>
+            <div className="header-links">
+                <a href="cart.html">Cart</a>
+                <a href="signin.html">Sign In</a>
+            </div>
+        </header>
+        <aside className="sidebar">
+            <h3 className="sidebar-title">Shopping Categories</h3>
+            <button className="sidebar-close-button" onclick="closeMenu()">x</button>
+            <ul>
+                <li>
+                    <a href="index.html">Fruites</a>
+                </li>
+
+                <li>
+                    <a href="index.html">Vegetables</a>
+                </li>
+            </ul>
+        </aside>
+      
+        <main className="main">
+            <div className="content">
+                <ul className="products">
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                    <li> 
+                        <div className="product">
+                            <img className="product-image" src="images/pineapple.jpg" alt="product"/>
+                            <div className="product-name">
+                                <a href="product.html">Pineapple</a>
+                            </div>
+                            <div className="product-brand">Solomon Island</div>
+                            <div className="product-price">$15</div>
+                            <div className="product-rating">4.5 Stars (10 Reviews)</div>
+
+                        </div>
+                    </li> 
+                </ul>
+            
+            </div>
+            
+        </main>
+        <footer className="footer">
+            All right reserved. 
+        </footer>
     </div>
   );
 }
