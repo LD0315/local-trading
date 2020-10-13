8a4aab8deb3a2b1a298d557e4fe73a931acb1b00
Linlin <linlin@d-i89-172-234.student.eduroam.uq.edu.au>
2020-10-13 10:14:05 +1000

    router, screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
 
 
 function App() {
@@ -13,6 +16,7 @@ function App() {
   }
 
   return (
+    <BrowserRouter>
     <div className="grid-container">
         <header className="header">
             <div className="brand">
@@ -42,28 +46,17 @@ function App() {
       
         <main className="main">
             <div className="content">
-                <ul className="products">
-                  {
-                    data.products.map(product => 
-                      <li> 
-                        <div className="product">
-                            <img className="product-image" src={product.image} alt="product" />
-                            <div className="product-name">
-                                <a href="product.html">{product.name} </a>
-                            </div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">${product.price}</div>
-                            <div className="product-rating">{product.rating} Stars {product.numReviews} Reviews </div>
-                        </div>
-                     </li>)
-                    }
-                </ul>
+                <Route path="/products/:id" component={ProductScreen} />
+                <Route path="/" exact={true} component={HomeScreen} />
+ 
+                
             </div>    
         </main>
         <footer className="footer">
             All right reserved. 
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
