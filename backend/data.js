e26172a6d4a9f0eb3ab75edfbb41937007294763
Linlin <linlin@d-i89-172-234.student.eduroam.uq.edu.au>
2020-10-14 14:17:08 +1000

    add babel

---
+++ backend/data.js
@@ -0,0 +1,64 @@
+export default {
+    products: [
+      {
+        _id: '1',
+        name: 'Pinenapple',
+        category: 'Fruits',
+        image: '/images/pineapple.jpg',
+        price: 15,
+        brand: 'Solomon Island',
+        rating: 4.5,
+        numReviews: 10
+     },
+     {
+        _id: '2',
+        name: 'Coconut',
+        category: 'Fruits',
+        image: '/images/pineapple.jpg',
+        price: 20,
+        brand: 'Solomon Island',
+        rating: 4.9,
+        numReviews: 15
+     },
+     {
+        _id: '3',
+        name: 'Avocado',
+        category: 'Vegetables',
+        image: '/images/pineapple.jpg',
+        price: 7,
+        brand: 'Solomon Island',
+        rating: 4.2,
+        numReviews: 8
+     },
+     {
+        _id: '4',
+        name: 'Watermelon',
+        category: 'Fruits',
+        image: '/images/pineapple.jpg',
+        price: 9,
+        brand: 'Solomon Island',
+        rating: 3.9,
+        numReviews: 12
+     },
+     {
+        _id: '5',
+        name: 'Eggplant',
+        category: 'Vegetables',
+        image: '/images/pineapple.jpg',
+        price: 6,
+        brand: 'Solomon Island',
+        rating: 4.4,
+        numReviews: 20
+     },
+     {
+        _id: '6',
+        name: 'Tomato',
+        category: 'Vegetables',
+        image: '/images/pineapple.jpg',
+        price: 10,
+        brand: 'Solomon Island',
+        rating: 5.0,
+        numReviews: 11
+     },
+]
+}
