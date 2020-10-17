5bd430195a6e8b281a6ec9fdb68620b9056e119c
Linlin <linlin@d-i89-172-234.student.eduroam.uq.edu.au>
2020-10-17 21:21:32 +1000

    update

--- frontend/src/Screens/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen.js
@@ -1,17 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { signin } from '../actions/userActions';
+import { register } from '../actions/userActions';
 
 
 function RegisterScreen(props) {
     
-    const [name, setEmail] = useState('');
+    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [rePassword, setRePassword] = useState('');
-    const userSignin = useSelector( state => state.userSignin );
-    const { loading, userInfo, error } = userSignin;
+    const userRegister = useSelector( state => state.userRegister);
+    const { loading, userInfo, error } = userRegister;
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -26,38 +26,50 @@ function RegisterScreen(props) {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        dispatch(signin(email, password));
+        dispatch(register(email, password));
     }
 
     return <div className="form">
         <form onSubmit={submitHandler} >
             <ul className="form-container">
                 <li>
-                    <h2>Sign-In</h2>
+                    <h2>Create Account</h2>
                 </li>
                 <li>
                     {loading && <div>Loading...</div>}
                     {error && <div>{error}</div>}
                 </li>
                 <li>
-                    <label for="email">Email</label>
+                    <label htmlFor="name">Name</label>
+                    <input type="name" name="name" id="name" onChange={(e) => setEmail(e.target.value)}>
+                    </input>
+                </li>
+
+                <li>
+                    <label htmlFor="email">Email</label>
                     <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)}>
                     </input>
                 </li>
+
                 <li>
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}>
                     </input>
                 </li>
+
                 <li>
-                    <button type="submit" className="button primary full-width">Sign in</button>
+                    <label htmlFor="rePassword">Re-Enter Password</label>
+                    <input type="rePassword" id="rePassword" name="rePassword" onChange={(e) => setPassword(e.target.value)}>
+                    </input>
+                </li>
+
+                <li>
+                    <button type="submit" className="button primary full-width">Register</button>
                 </li>
                 <li>
-                    New to LOCAL TRADING?
+                    Already have an account? <Link to="/signin">Sign-In</Link>
                 </li>
-                <li>
-                  <Link to="/register" className="button secondary full-width text-center">Create yout local trading account</Link>
-                </li>
+                
             </ul>
 
         </form>
